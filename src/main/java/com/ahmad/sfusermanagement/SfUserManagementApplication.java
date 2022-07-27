df466a13635ec6df3467bb14f6140771ce6b2322
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-27 22:50:46 +0500

    Initial Commit.

---
+++ src/main/java/com/ahmad/sfusermanagement/SfUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.sfusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfUserManagementApplication.class, args);
+	}
+
+}
