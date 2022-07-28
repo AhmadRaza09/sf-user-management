31c92f35cb636e2cb204861ff0ceb3f6953d9d69
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-28 11:04:45 +0500

    added pojos for user and role. closes #1.

---
+++ src/main/java/com/ahmad/sfusermanagement/model/Role.java
@@ -0,0 +1,14 @@
+package com.ahmad.sfusermanagement.model;
+
+public class Role extends BaseEntity{
+
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/ahmad/sfusermanagement/model/User.java
@@ -0,0 +1,33 @@
+package com.ahmad.sfusermanagement.model;
+
+import java.time.LocalDate;
+
+public class User extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
